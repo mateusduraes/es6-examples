9b02f8655053e182ac8a06840b3a94c2077bb4da
mduraes1994 <mduraes1994@gmail.com>
2017-08-09 10:51:28 -0300

    new string methods example

---
+++ 09 - New Strings Methods/string-method-01.js
@@ -0,0 +1,8 @@
+const text = 'Mussum Ipsum, cacilds vidis litro abertis.';
+
+console.log(text.startsWith('Mussum'));
+console.log(text.startsWith('um Ipsum' ,4));
+console.log(text.endsWith('abertis.'));
+console.log(text.endsWith('aberti', 40));
+
+console.log(text.repeat(2));
